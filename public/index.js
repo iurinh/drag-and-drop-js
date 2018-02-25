67f09946d1f5d0a978dc604ab87aeb8b96cb2373
emy <emy.fm@hotmail.com>
2018-02-25 20:51:32 -0300

    By Iuri: Adicionando evento em varios componentes

--- public/index.js
+++ public/index.js
@@ -2,30 +2,47 @@
 
 // Componentes de tela
 var body = document.querySelector('body');
-var bloco = document.querySelector('.bloco');
+var blocos = document.querySelectorAll('#bloco');
+var tiras = document.querySelectorAll('#tira');
 var campo = document.querySelector('.campo');
 var mensagem = document.querySelector('.mensagem');
 
-var lastComponentClicked;
-
-/** Eventos para os componentes de tela que funcionarão como drag-and-drop */
+/** Adiciona evento para os componentes de tela que funcionarão como drag-and-drop */
 function adicionarMovimento(component){
     component.addEventListener('mousedown', function(event){
+        component.classList.add('absolute');
+        component.classList.add('elevar');
+
         component.addEventListener('mousemove', service.mover);
+
+        event.stopPropagation();
     });
 
     component.addEventListener('touchstart', function(event){
+        component.classList.add('absolute');
+        component.classList.add('elevar');
+
         component.addEventListener('touchmove', service.mover);
+
+        event.stopPropagation();
     });
 
     component.addEventListener('mouseup', function(event){
+        component.classList.remove('elevar');
+
         component.removeEventListener('mousemove', service.mover);
         validarPosicaoFinal(event);
+
+        event.stopPropagation();
     });
 
     component.addEventListener('touchend', function(event){
+        component.classList.remove('elevar');
+
         component.removeEventListener('touchmove', service.mover);
         validarPosicaoFinal(event);
+
+        event.stopPropagation();
     });
 }
 
@@ -56,5 +73,12 @@ function validarMovimentoCursor(event){
 }
 
 // Inicializadores
-adicionarMovimento(bloco);
+blocos.forEach(function(bloco){
+    adicionarMovimento(bloco); 
+});
+
+tiras.forEach(function(tira){
+    adicionarMovimento(tira); 
+});
+
 inspecinarCursor();
